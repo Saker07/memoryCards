e5753403688a651db7bc335a6351e1de2674707f
Saker07 <gjoni.klaudio@gmail.com>
2023-04-28 02:31:25 +0100

    Add showHitCards list, json with all available cards, and have smaller random selection of cards at component mount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {useState, useEffect} from 'react';
+import json from "./cards.json";
 
 function App() {
   let [cards, setCards] = useState([]);
   let [currScore, setCurrScore] = useState(0);
   let [bestScore, setBestScore] = useState(0);
+  
+  const numberOfCards = 6;
 
   let handleClick = (id)=>{
     //card with correct id, make it hit=true if false, otherside all hit in cards set to false and currscore set to 0
-    if(cards[id].hit){
+    let foundIndex = cards.findIndex(x=>x.id === id);
+    if(cards[foundIndex].hit){
       setCurrScore(0);
     } else{
       setCards((prev)=>{
         let newArr = [...prev];
-        newArr[id].hit = true;
+        newArr[foundIndex].hit = true;
         return newArr;
       });
       setCurrScore((prev)=>{return prev+1});
@@ -23,7 +27,6 @@ function App() {
     let newArr = [...arr];
 
     let m = newArr.length;
-    let i;
     while(m){
       let i = Math.floor(Math.random() *  m--);
 
@@ -35,9 +38,11 @@ function App() {
     return newArr;
 
   }
+
   useEffect(()=>{
-    setCards([{id: 0, hit: false},{id: 1, hit: false},{id: 2, hit: false},{id: 3, hit: false},{id: 4, hit: false},{id: 5, hit: false}])
-    console.log('mount')
+    //setCards([{id: 0, hit: false, eng: 'back', hira: 'back', kata: 'altBack'},{id: 1, hit: false, eng: 'back', hira: 'back', kata: 'altBack'},{id: 2, hit: false, eng: 'back', hira: 'back', kata: 'altBack'},{id: 3, hit: false, eng: 'back', hira: 'back', kata: 'altBack'},{id: 4, hit: false, eng: 'back', hira: 'back', kata: 'altBack'},{id: 5, hit: false, eng: 'back', hira: 'back', kata: 'altBack'}])
+    setCards(createShuffledCopy([...json.cards]).slice(0, numberOfCards))
+    console.log('mount');
   }, []);
   useEffect(()=>{
     if(currScore === 0){
@@ -52,9 +57,12 @@ function App() {
 
   return (
     <div className="game">
-      {createShuffledCopy(cards).map(element=>{
-        return <div onClick={()=>{handleClick(element.id)}}>/-card_{element.id}_{element.hit.toString()}-/</div>
-      })}
+      <div class='showHitCards'>{cards.filter(elem=>elem.hit === true).map(elem=><b>{elem.id}</b>)}</div>
+      <div class='cardsHolder'>
+        {createShuffledCopy(cards).map(element=>{
+          return <div className='card' onClick={()=>{handleClick(element.id)}}>/-card_{element.id}_{element.hit.toString()}-/</div>
+        })}
+      </div>
       <div>{currScore}</div>
       <div>{bestScore}</div>
     </div>
